a8c72d756181c695f3bd19b2465d3942217dcb66
leticiar-io <leticia.rodrigues20019@gmail.com>
2024-05-07 16:51:55 -0300

    :tada: Finalizando o primeiro projeto da jornada de Python

--- codigo.py
+++ codigo.py
@@ -24,9 +24,10 @@ pyautogui.press("enter")
 # 2. Fazer login
 time.sleep(3)
 # print(pyautogui.position()) # descobrir a posição do botão de login
-pyautogui.click(x=1407, y=401)
-pyautogui.write("leticia.rodrigues20021@gmail.com")
+#pyautogui.click(x=592, y=831)
 pyautogui.press("tab")
+pyautogui.write("leticia.rodrigues20021@gmail.com")
+pyautogui.press("tab")     
 
 pyautogui.write("123456")
 pyautogui.press("tab")
@@ -39,14 +40,19 @@ time.sleep(3)
 
 import pandas as pd
 
-tabela = pd.read_csv("produtos.csv") # Importar a base de dados
+tabela = pd.read_csv("produtos.csv") # Impo1 25.95 6.5   nanrtar a base de dados
 
+# print(tabela)
+
+pyautogui.press("tab")
 # 4. Cadastrar produtos
-for linha in tabela.index: # index -> retorna a quantidade de linhas da tabela 
-   pyautogui.click(x=1334, y=282)
+for linha in tabela.index: # index -> retorna a quantidade de linhas da tabela // e se eu quisesse as colunas seria tabela.columns
+   pyautogui.click(x=1325, y=325)
    pyautogui.write(str(tabela.loc[linha, "codigo"])) # loc é de localizar
    pyautogui.press("tab")
    pyautogui.write(str(tabela.loc[linha, "marca"]))
+   pyautogui.press("tab")
+   pyautogui.write(str(tabela.loc[linha, "tipo"]))
    pyautogui.press("tab")
    pyautogui.write(str(tabela.loc[linha, "categoria"]))
    pyautogui.press("tab")
@@ -55,12 +61,14 @@ for linha in tabela.index: # index -> retorna a quantidade de linhas da tabela
    pyautogui.write(str(tabela.loc[linha, "custo"]))
    pyautogui.press("tab")
 
-   if not pd.isna(tabela.loc[linha, "obs"]): 
-      pyautogui.white(str(tabela.loc[linha, "obs"]))
+   obs = str(tabela.loc[linha, "obs"])
+
+   obs = tabela.loc[linha, "obs"]
+   if not pd.isna(obs):
+      pyautogui.write(str(tabela.loc[linha, "obs"]))
+  
    pyautogui.press("tab")
    pyautogui.press("enter")
-   pyautogui.scroll(5000) # Rolar a tela para cima
-
-
-
-# 5. Repetir isso tudo até acabar a lista de produtos
+    # dar scroll de tudo pra cima
+   pyautogui.scroll(5000)
+print("Fim do cadastro de produtos")
